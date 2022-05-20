ebf1656b688dc1e2f467673e26dc765db79c12aa
hediye gül aydın <hediyegulaydin412@gmail.com>
2022-05-20 22:53:56 +0300

    writer ındex oluşturuldu

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,24 @@ namespace CoreDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSession();
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +59,8 @@ namespace CoreDemo
             }
             app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
             app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
 
             app.UseRouting();
 
