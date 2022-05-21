3ae86c2426b934354704c121b9fa70e437066af0
hediye gül aydın <hediyegulaydin412@gmail.com>
2022-05-22 01:06:04 +0300

    blog add part1 tamamlandı

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DAL.EntityFramework;
 using EntityKatmani.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CoreDemo.Controllers
 {
@@ -35,7 +38,24 @@ namespace CoreDemo.Controllers
         [HttpPost]
         public IActionResult BlogAdd(Blog p)
         {
-            return View();
+            BlogValidator bv =new BlogValidator();
+			ValidationResult results =bv.Validate(p);
+			if (results.IsValid)
+			{
+				p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+				p.WriterID=2;
+                bm.TAdd(p);
+				return RedirectToAction("BlogListByWriter", "Blog");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
         }
     }
 }
