8cb6cabf16f9e167f0cfd99a20bfa0bdc63d003a
hediye gül aydın <hediyegulaydin412@gmail.com>
2022-05-25 01:03:09 +0300

    ajax işlemleri tamamlandı

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -16,6 +17,32 @@ namespace CoreDemo.Areas.Admin.Controllers
         {
             var jsonWriters = JsonConvert.SerializeObject(writers);
             return Json(jsonWriters);
+        }
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter=writers.FirstOrDefault(x=>x.Id==writerid);
+            var jsonWriters=JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer=writers.FirstOrDefault(x=>x.Id== id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
         }
 
         public static List<WriterClass> writers = new List<WriterClass>()
@@ -33,7 +60,7 @@ namespace CoreDemo.Areas.Admin.Controllers
             },
             new WriterClass
             {
-                Id = 1,
+                Id = 3,
                 Name="Buse"
             }
         };
