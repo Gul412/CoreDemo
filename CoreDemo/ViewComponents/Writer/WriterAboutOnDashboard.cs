316db3146bf3f4904642dce5e214572828222581
hediye gül aydın <hediyegulaydin412@gmail.com>
2022-05-26 13:54:10 +0300

    dashboard düzenlemeleri #1

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DAL.Concrete;
 using DAL.EntityFramework;
+using EntityKatmani.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -9,14 +10,19 @@ namespace CoreDemo.ViewComponents.Writer
         public class WriterAboutOnDashboard : ViewComponent
         {
             WriterManager writermanager = new WriterManager(new EfWriterRepository());
-            public IViewComponentResult Invoke()
-            {
-            var usermail = User.Identity.Name;
             Context c = new Context();
-            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID)
-                .FirstOrDefault();
+
+        public IViewComponentResult Invoke()
+            {
+            var username = User.Identity.Name;
+            ViewBag.veri = username;
+           var usermail=c.Users.Where(x => x.UserName == username)
+                .Select(y=>y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail)
+             .Select(y => y.WriterID).FirstOrDefault();
             var values = writermanager.GetWriterById(writerID);
-                return View(values);
-            }
-        }  
+            return View(values);
+        }
+       
+    }  
 }
