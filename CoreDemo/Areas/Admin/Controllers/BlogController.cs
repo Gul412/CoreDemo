53acb0c5251ce727abe42722754949a107d8fbaf
hediye gül aydın <hediyegulaydin412@gmail.com>
2022-05-27 01:50:43 +0300

    admin düzenlemeleri

---
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DAL.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        BlogManager blogmanager = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = blogmanager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
